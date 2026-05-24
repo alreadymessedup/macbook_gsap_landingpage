8ef51374e16e6700746963b8827bc11f748770b0
Riddhima <riddhima643@gmail.com>
2026-05-25 03:05:09 +0530

    Implement 3D product viewer

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,16 +3,22 @@ import { useMacbookStore } from '../store/index'
 import clsx from 'clsx'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-
+import MacbookModel14 from './models/Macbook-14'
+import MacbookModel16 from './models/Macbook-16'
+import StudioLights from './three/studiolights'
+import ModelSwitcher from './three/ModelSwitcher'
+import { useMediaQuery } from 'react-responsive'
 
 const ProductViewer = () => {
     const {color, scale, setcolor, setScale, reset} = useMacbookStore();
+
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px'});
   return (
     <section id='product-viewer'>
         <h2>Take a closer Look</h2>
 
         <div className='controls'>
-            <p className='info'>MacBookPro 16" in {color}</p>
+            <p className='info'>MacBook Pro | Available in 14" & 16" in Space Gray & Dark </p>
 
             <div className='flex-center gap-5 mt-5'>
                 <div  className='color-control'>
@@ -44,12 +50,10 @@ const ProductViewer = () => {
 
         </div>
         <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
-  <mesh position={[0, 0, 0]} scale={10 * scale}>
-    <boxGeometry />
-    <meshStandardMaterial color={color} />
-  </mesh>
+            <StudioLights />
+ 
 
-  <OrbitControls enableZoom={false} />
+  <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
 </Canvas>
 
     </section>
---
+++ src/components/three/studiolights.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Environment, Lightformer } from '@react-three/drei'
+import { Group } from 'three'
+
+const studiolights = () => {
+  return (
+    <group name='lights'>
+        <Environment resolution={256}>
+            <group>
+                <Lightformer form="rect" intensity={10} position={[-10, 5, -5]} scale={10} rotation-y={Math.PI / 2}/>
+                <Lightformer form="rect" intensity={10} position={[10, 0, 1]} scale={10} rotation-y={Math.PI / 2}/>  
+            </group> 
+        </Environment>
+        <spotLight intensity={Math.PI * 1}   position={[0, 15, 5]}   decay={0.1} angle={0.15} />
+        <spotLight intensity={Math.PI * 0.2} position={[0, -25, 10]} decay={0}   angle={0.15} />
+    </group>
+  )
+}
+
+export default studiolights
